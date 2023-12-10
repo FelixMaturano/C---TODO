3445a961365b76dcfb4f30e346d2a2b28122dbdf
Felix Maturano Zarate <maturanozaratefelix@usfx.bo>
2023-12-10 01:18:02 -0400

    Python_inicios

---
+++ punterosE1.cpp
@@ -0,0 +1,25 @@
+/* Ejercicio 1: Comprobar si un numero es par o impar, y senalar la pocision de la memoria 
+donde se esta guardando el numero. Con punteros.*/
+
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+   // int numero, *dir_numero;
+    cout << "Digite un numero :";
+    // cin>>numero; // Pedimos el numero al usuario
+
+    // dir_numero = &numero;
+
+    // if(*dir_numero%2==0)
+    // {
+    //     cout<<"El  numero : "<<*dir_numero<<"es par"<<endl;
+    //     cout<<"Pocision: "<<dir_numero<<endl;
+    // }else{
+    //     cout<<"El numero:"<<*dir_numero<<"es impar"<<endl;
+    //     cout<<"Pocision: "<<dir_numero<<endl;
+    // }
+    // return 0;
+}
